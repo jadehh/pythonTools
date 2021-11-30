e7f0420ede8e032fdb31a0ccc440ea2d0f74e7d5
jade <jadehh@live.com>
2021-11-30 10:59:12 +0800

    update __init__.py

--- jade/__init__.py
+++ jade/__init__.py
@@ -1,12 +1,12 @@
 #coding=utf-8
+AppRunPath = "jade/AppRun"
+DIRECTORY_ANNOTATIONS = 'Annotations/'
+DIRECTORY_IMAGES = 'JPEGImages/'
+DIRECTORY_PREANNOTATIONS = "PredictAnnotations/"
 from jade.jade_logging import *
 from jade.jade_packing import *
 from jade.jade_tools import *
 from jade.jade_sqlite_data_base import *
 from jade.jade_progress_bar import *
 
-AppRunPath = "jade/AppRun"
-DIRECTORY_ANNOTATIONS = 'Annotations/'
-DIRECTORY_IMAGES = 'JPEGImages/'
-DIRECTORY_PREANNOTATIONS = "PredictAnnotations/"
 
