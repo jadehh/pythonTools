c801ac39ab21f4dedd19e62dac5a3ab2c8e03f09
jade <jadehh@live.com>
2021-11-30 13:26:25 +0800

    update init py

--- opencv_tools/__init__.py
+++ opencv_tools/__init__.py
@@ -2,6 +2,9 @@
 import os
 file_path = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
 font_path = "{}/jade_simhei.ttf".format(file_path)
+DIRECTORY_ANNOTATIONS = 'Annotations/'
+DIRECTORY_IMAGES = 'JPEGImages/'
+DIRECTORY_PREANNOTATIONS = "PredictAnnotations/"
 from opencv_tools.jade_visualize import *
 
 
