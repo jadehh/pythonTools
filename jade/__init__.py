32d500ebce55cf4f02ebcc55ad621c410591b937
jade <jadehh@live.com>
2021-11-30 13:26:05 +0800

    update init py

--- jade/__init__.py
+++ jade/__init__.py
@@ -2,9 +2,6 @@
 import os
 file_path = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
 AppRunPath = "/AppRun".format(file_path)
-DIRECTORY_ANNOTATIONS = 'Annotations/'
-DIRECTORY_IMAGES = 'JPEGImages/'
-DIRECTORY_PREANNOTATIONS = "PredictAnnotations/"
 from jade.jade_logging import *
 from jade.jade_packing import *
 from jade.jade_tools import *
