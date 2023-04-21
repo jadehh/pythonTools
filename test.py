f6e5d76ad566d0fdddfa1726700fc499b1b732e4
jade <jadehh@live.com>
2023-04-21 10:11:32 +0800

    * 如果当前登录的加密狗超过最大限制数量,在查找是否还有新的加密狗

--- test.py
+++ test.py
@@ -7,10 +7,18 @@
 # @Software : Samples
 # @Desc     :
 from jade import *
+from jade.jade_threading import MonitorLDKThread
+from pyldk.pyldk import PyLdk
+
 def test_process_bar():
     progressBar = ProgressBar(10)
     for i in range(10):
         time.sleep(1)
         progressBar.update()
+def test_monitor_pydk():
+    JadeLog  = JadeLogging("log",Level="DEBUG")
+    ldkqueue = Queue(10)
+    LDKREFRESHTIME = 30
+    MonitorLDKThread(PyLdk(JadeLog), JadeLog, ldkqueue, LDKREFRESHTIME)
 if __name__ == '__main__':
-    test_process_bar()
+    test_monitor_pydk()
