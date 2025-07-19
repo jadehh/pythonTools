76135a9f02b9a68ea577dfeb3f692c4b31807551
jade <jadehh@live.com>
2025-07-19 23:50:18 +0800

    * 去除不编译的文件，包括其import的环境也不写入

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 
 ```bash
 python main.py --dataset_type paddle_detection  --input_dataset_dir F:\数据集\VOC数据集\箱门检测数据集\ContainVOC  --save_dataset_dir E:\Data\VOC数据集\箱门检测数据集\ContainVOC
+python main.py --dataset_type paddle_text_detection --input_dataset_dir F:\数据集\关键点检测数据集\箱号关键点数据集 --save_dataset_dir E:\Data\关键点检测数据集\箱号关键点数据集
 ```
--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from dataset_tools.jade_create_object_dection_datasets import CreateYearsDataset
 def test_create_paddle_years_datasets(args):
     CreateYearsDatasets(args.input_dataset_dir,None,save_path=args.save_dataset_dir,rate=0.9)
 
+
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description="制作数据集脚本")
@@ -26,6 +27,8 @@ if __name__ == '__main__':
         CreateYearsDatasets(args.input_dataset_dir, None, save_path=args.save_dataset_dir, rate=0.9)
     elif args.dataset_type == "yolo_detection":
         CreateDarknetVocDatasets(args.input_dataset_dir,  save_path=args.save_dataset_dir, rate=0.9, VOC_CLASSES=args.voc_labels)
+    elif args.dataset_type == "paddle_text_detection":
+        create_text_detection_datasets(args.input_dataset_dir, args.save_dataset_dir, split_rate=0.95)
 
     #removeNolabelDatasets(r"F:\数据集\关键点检测数据集\定制版箱号关键点数据集\2022-03-09")
     #create_text_detection_datasets(r"F:\数据集\关键点检测数据集\定制版箱号关键点数据集",r"E:\Data\字符检测识别数据集\定制版箱号关键点数据集",0.95)
